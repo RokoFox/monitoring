c6ad7464b236d927b529561a8d4a6f12e6f44fc1
Roko Lisica <roko.lisica@fer.hr>
2017-05-13 17:37:37 +0200

    "Do nothing" pseudoaction added to the config menu.

--- app/src/main/java/monitoring/seminar/fer/monitoring/ConfigureActivity.java
+++ app/src/main/java/monitoring/seminar/fer/monitoring/ConfigureActivity.java
@@ -3,24 +3,24 @@ package monitoring.seminar.fer.monitoring;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class ConfigureActivity extends AppCompatActivity {
     private static final String TAG = "ConfigureActivity";
+    private static final String DO_NOTHING = "DO_NOTHING";
 
-    private Spinner spFaceDetected1;
-    private Spinner spFaceDetected2;
-
-    private Spinner spNoiseDetected1;
-    private Spinner spNoiseDetected2;
+    private Set<Spinner> mFaceSpinners = new HashSet<>();
+    private Set<Spinner> mNoiseSpinners = new HashSet<>();
 
 
 
@@ -30,92 +30,96 @@ public class ConfigureActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_configure);
 
-        spFaceDetected1 = (Spinner) findViewById(R.id.spFaceDetected1);
-        spFaceDetected2 = (Spinner) findViewById(R.id.spFaceDetected2);
+        mFaceSpinners.add((Spinner) findViewById(R.id.spFaceDetected1));
+        mFaceSpinners.add((Spinner) findViewById(R.id.spFaceDetected2));
 
-        spNoiseDetected1 = (Spinner) findViewById(R.id.spNoiseDetected1);
-        spNoiseDetected2 = (Spinner) findViewById(R.id.spNoiseDetected2);
+        mNoiseSpinners.add((Spinner) findViewById(R.id.spNoiseDetected1));
+        mNoiseSpinners.add((Spinner) findViewById(R.id.spNoiseDetected2));
+
+        List<String> allActions = new LinkedList<>(Arrays.asList(Actions.ALL));
+        allActions.add(0, DO_NOTHING);
 
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_spinner_item,
-                Actions.ALL);
+                allActions
+        );
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
 
-        spFaceDetected1.setAdapter(spinnerAdapter);
-        spFaceDetected2.setAdapter(spinnerAdapter);
+        for (Spinner spinner : mFaceSpinners) {
+            spinner.setAdapter(spinnerAdapter);
+        }
 
-        spNoiseDetected1.setAdapter(spinnerAdapter);
-        spNoiseDetected2.setAdapter(spinnerAdapter);
+        for (Spinner spinner : mNoiseSpinners) {
+            spinner.setAdapter(spinnerAdapter);
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        saveCameraPrefs();
-        saveSoundPrefs();
+        savePrefs(mFaceSpinners, MonitoringActivity.CAMERA_TRIGGERED_PREFS, ActionTriggers.FACE_DETECTED);
+        savePrefs(mNoiseSpinners, MonitoringActivity.SOUND_TRIGGERED_PREFS, ActionTriggers.NOISE_DETECTED);
     }
 
-    private void saveCameraPrefs() {
-        SharedPreferences.Editor prefs = getSharedPreferences(MonitoringActivity.CAMERA_TRIGGERED_PREFS,
+    private void savePrefs(Set<Spinner> spinners, String fileName, String triggerName) {
+        SharedPreferences.Editor prefs = getSharedPreferences(fileName,
                 Context.MODE_PRIVATE).edit();
 
-        Set<String> faceDetectedActions = new HashSet<>();
+        Set<String> actions = new HashSet<>();
 
-        String selectedItem = (String) spFaceDetected1.getSelectedItem();
-        if (selectedItem != null) {
-            faceDetectedActions.add(selectedItem);
-        }
+        String selectedItem;
+        for (Spinner spinner : spinners) {
+            selectedItem = (String) spinner.getSelectedItem();
 
-        selectedItem = (String) spFaceDetected2.getSelectedItem();
-        if (selectedItem != null) {
-            faceDetectedActions.add(selectedItem);
-        }
-
-        Log.d(TAG, "" + faceDetectedActions + " added to preferences");
-
-        if (! faceDetectedActions.isEmpty()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                prefs.putStringSet(ActionTriggers.FACE_DETECTED, faceDetectedActions);
-            } else {
-                prefs.putString(ActionTriggers.FACE_DETECTED,
-                        Serialization.serializeStrings(faceDetectedActions));
+            if (selectedItem != null && ! selectedItem.equals(DO_NOTHING)) {
+                actions.add(selectedItem);
             }
+        }
+
+        Log.d(TAG, "" + actions + " added to preferences");
+
+        if (! actions.isEmpty()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                prefs.putStringSet(triggerName, actions);
+            } else {
+                prefs.putString(triggerName,
+                        Serialization.serializeStrings(actions));
+            }
+        } else {
+            prefs.remove(triggerName);
         }
 
         prefs.apply();
     }
 
-    private void saveSoundPrefs() {
-        SharedPreferences.Editor prefs = getSharedPreferences(MonitoringActivity.SOUND_TRIGGERED_PREFS,
-                Context.MODE_PRIVATE).edit();
-
-        Set<String> noiseDetectedActions = new HashSet<>();
-
-        String selectedItem = (String) spNoiseDetected1.getSelectedItem();
-        if (selectedItem != null) {
-            noiseDetectedActions.add(selectedItem);
-        }
-
-        selectedItem = (String) spNoiseDetected2.getSelectedItem();
-        if (selectedItem != null) {
-            noiseDetectedActions.add(selectedItem);
-        }
-
-        Log.d(TAG, "" + noiseDetectedActions + " added to preferences");
-
-        if (! noiseDetectedActions.isEmpty()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                prefs.putStringSet(ActionTriggers.NOISE_DETECTED, noiseDetectedActions);
-            } else {
-                prefs.putString(ActionTriggers.NOISE_DETECTED,
-                        Serialization.serializeStrings(noiseDetectedActions));
-            }
-        }
-
-        prefs.apply();
-    }
-
-
+//    private void saveSoundPrefs() {
+//        SharedPreferences.Editor prefs = getSharedPreferences(MonitoringActivity.SOUND_TRIGGERED_PREFS,
+//                Context.MODE_PRIVATE).edit();
+//
+//        Set<String> noiseDetectedActions = new HashSet<>();
+//
+//        String selectedItem = (String) spNoiseDetected1.getSelectedItem();
+//        if (selectedItem != null) {
+//            noiseDetectedActions.add(selectedItem);
+//        }
+//
+//        selectedItem = (String) spNoiseDetected2.getSelectedItem();
+//        if (selectedItem != null) {
+//            noiseDetectedActions.add(selectedItem);
+//        }
+//
+//        Log.d(TAG, "" + noiseDetectedActions + " added to preferences");
+//
+//        if (! noiseDetectedActions.isEmpty()) {
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//                prefs.putStringSet(ActionTriggers.NOISE_DETECTED, noiseDetectedActions);
+//            } else {
+//                prefs.putString(ActionTriggers.NOISE_DETECTED,
+//                        Serialization.serializeStrings(noiseDetectedActions));
+//            }
+//        }
+//
+//        prefs.apply();
 }
